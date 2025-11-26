3b0782c03362a55a1cbc9548f03efbc922af90dc
Carson <carson@CarsondeMacBook-Air.local>
2025-11-26 20:02:40 +0800

    1126 commit

--- src/main.cpp
+++ src/main.cpp
@@ -540,15 +540,439 @@ ListNode *removeNthFromEnd2(ListNode *head, int n) {
     return newNode->next;
 }
 
-int main() {
-    ListNode *head = new ListNode(1);
-    // while (head != NULL) {
-    //     cout << head->val << endl;
-    //     head = head->next;
-    // }
-    ListNode *newNode = removeNthFromEnd(head, 1);
-    while (newNode != NULL) {
-        cout << newNode->val << endl;
-        newNode = newNode->next;
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    if (headA == NULL || headB == NULL) {
+        return NULL;
     }
+
+    unordered_set<ListNode *> nodesA;
+    unordered_set<ListNode *> nodesB;
+
+    while (headA != NULL) {
+        nodesA.insert(headA);
+        headA = headA->next;
+    }
+
+    while (headB != NULL) {
+        if (nodesA.find(headB) != nodesA.end()) {
+            return headB;
+        }
+        headB = headB->next;
+    }
+
+    return NULL;
+}
+
+ListNode *detectCycle(ListNode *head) {
+    if (head == NULL || head->next == NULL) {
+        return NULL;
+    }
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            ListNode *tmp1 = fast;
+            ListNode *tmp2 = head;
+            while (tmp2 != tmp1) {
+                tmp2 = tmp2->next;
+                tmp1 = tmp1->next;
+            }
+
+            return tmp2;
+        }
+    }
+
+    return NULL;
+}
+
+bool isAnagram(string s, string t) {
+    vector<char> result(26, 0);
+    for (int i = 0; i < s.length(); i++) {
+        result[s[i] - 'a']++;
+    }
+    for (int i = 0; i < t.length(); i++) {
+        result[t[i] - 'a']--;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (result[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isAnagram2(string s, string t) {
+    unordered_map<char, int> s_map;
+    unordered_map<char, int> t_map;
+    for (char c : s) {
+        s_map[c]++;
+    }
+    for (char c : t) {
+        t_map[c]++;
+    }
+    for (auto it : s_map) {
+        if (t_map.find(it.first) == t_map.end()) {
+            return false;
+        } else {
+            if (s_map[it.first] != t_map[it.first]) {
+                return false;
+            }
+            t_map.erase(it.first);
+        }
+    }
+    return t_map.size() == 0 ? true : false;
+}
+
+bool canConstruct(string ransomNote, string magazine) {
+    vector<int> result(26, 0);
+    for (char c : magazine) {
+        result[c - 'a']++;
+    }
+    for (char c : ransomNote) {
+        result[c - 'a']--;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (result[i] < 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> result;
+    for (string s : strs) {
+        string tmp = s;
+        sort(tmp.begin(), tmp.end());
+        result[tmp].push_back(s);
+    }
+
+    vector<vector<string>> return_result;
+    for (auto it : result) {
+        return_result.push_back(it.second);
+    }
+
+    return return_result;
+}
+
+vector<int> findAnagrams(string s, string p) {
+    vector<int> result;
+    sort(p.begin(), p.end());
+    int length = p.length();
+    int left = 0;
+    int right = left + length - 1;
+    for (; left < s.length() && right < s.length(); left++) {
+        string tmp = s.substr(left, length);
+        sort(tmp.begin(), tmp.end());
+        if (tmp == p) {
+            result.push_back(left);
+        }
+    }
+
+    return result;
+}
+
+int maxVowels(string s, int k) {
+    int left = 0, right = k - 1;
+    int i = left;
+    int count = 0;
+    while (i <= right) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            count++;
+        }
+        i++;
+    }
+    int max = count;
+    while (++right < s.length()) {
+        if (s[left] != 'a' && s[left] != 'e' && s[left] != 'i' && s[left] != 'o' && s[left] != 'u') {
+            if (s[right] == 'a' || s[right] == 'e' || s[right] == 'i' || s[right] == 'o' || s[right] == 'u') {
+                count++;
+            }
+        } else {
+            if (s[right] != 'a' && s[right] != 'e' && s[right] != 'i' && s[right] != 'o' && s[right] != 'u') {
+                count--;
+            }
+        }
+        max = max > count ? max : count;
+    }
+
+    return max;
+}
+
+bool test(unordered_map<char, int> &s_map, unordered_map<char, int> &t_map) {
+    for (auto it : s_map) {
+        cout << it.first << " " << it.second << endl;
+    }
+    for (auto it : s_map) {
+        cout << it.first << " " << it.second << endl;
+        if (t_map.find(it.first) == t_map.end()) {
+            cout << "aaaa" << endl;
+            return false;
+        } else {
+            if (t_map[it.first] != it.second) {
+                return false;
+                cout << "222" << endl;
+            }
+        }
+    }
+
+    cout << "ccc" << endl;
+    return true;
+}
+
+vector<int> findAnagrams2(string s, string p) {
+    int p_length = p.length();
+    vector<int> result;
+    unordered_map<char, int> p_map;
+    unordered_map<char, int> s_map;
+    for (char i : p) {
+        p_map[i]++;
+    }
+    int left = 0, right = p_length - 1;
+    while (left < p_length) {
+        s_map[s[left++]]++;
+    }
+    left = 0;
+    if (s_map == p_map) {
+        result.push_back(left);
+    }
+    right = p_length - 1;
+    cout << "sada" << endl;
+    for (auto it : s_map) {
+        cout << it.first << " " << it.second << endl;
+    }
+    while (++right < s.length()) {
+        s_map[s[left]]--;
+        if (s_map[s[left]] == 0) {
+            s_map.erase(s[left]);
+        }
+        s_map[s[right]]++;
+        if (s_map == p_map) {
+            result.push_back(left + 1);
+        }
+        left++;
+    }
+
+    return result;
+}
+
+vector<int> findAnagrams3(string s, string p) {
+    int p_length = p.length();
+    array<int, 26> cnt_p{};
+    array<int, 26> cnt_s{};
+    vector<int> result;
+    for (char i : p) {
+        cnt_p[i - 'a']++;
+    }
+    int right = 0;
+    while (right < s.length()) {
+        int left = right + 1 - p_length;
+        cnt_s[s[right++] - 'a']++;
+        if (left < 0) {
+            continue;
+        }
+        if (cnt_s == cnt_p) {
+            result.push_back(left);
+        }
+        cnt_s[s[left] - 'a']--;
+    }
+
+    return result;
+}
+
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> result;
+    unordered_set<int> s1(nums1.begin(), nums1.end());
+    for (auto it : nums2) {
+        if (s1.find(it) != s1.end()) {
+            result.push_back(it);
+        }
+    }
+
+    return result;
+}
+
+vector<int> intersect2(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int> map1;
+    vector<int> result;
+    for (int i : nums1) {
+        map1[i]++;
+    }
+
+    for (int i : nums2) {
+        if (map1.find(i) != map1.end()) {
+            result.push_back(i);
+            map1[i]--;
+            if (map1[i] == 0) {
+                map1.erase(i);
+            }
+        }
+    }
+
+    return result;
+}
+
+int getSum(int n) {
+    int sum = 0;
+    while (n != 0) {
+        sum += (n % 10) * (n % 10);
+        n /= 10;
+    }
+
+    return sum;
+}
+
+bool isHappy(int n) {
+    unordered_set<int> s1;
+    while (1) {
+        int sum = getSum(n);
+        if (sum == 1) {
+            return true;
+        }
+        if (s1.find(sum) != s1.end()) {
+            return false;
+        } else {
+            s1.insert(sum);
+        }
+        n = sum;
+    }
+}
+
+int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+    unordered_map<int, int> map1;
+    unordered_map<int, int> map2;
+    int result = 0;
+    for (int i : nums1) {
+        for (int j : nums2) {
+            map1[i + j]++;
+        }
+    }
+
+    for (int i : nums3) {
+        for (int j : nums4) {
+            map2[i + j]++;
+        }
+    }
+
+    for (auto it : map1) {
+        if (map2.find(0 - it.first) != map2.end()) {
+            result += it.second * map2[0 - it.first];
+        }
+    }
+
+    return result;
+}
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++) {
+        int left = i + 1;
+        int right = nums.size() - 1;
+        if (i > 0 &&  nums[i] == nums[i - 1]) {
+            continue;
+        }
+        while (left < nums.size() && left < right) {
+            cout << "i " << i << "left " << left << " , right " << right << endl;
+            int tmp = nums[i] + nums[left] + nums[right];
+            if (tmp == 0) {
+                result.push_back({nums[i], nums[left], nums[right]});
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (tmp < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
+
+void reverseString(vector<char>& s) {
+    int left = 0, right = s.size() - 1;
+    while (left < right) {
+        char tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        left++;
+        right--;
+    }
+}
+
+string reverseStr(string s, int k) {
+    int left = 0, right = k - 1;
+    int p, q;
+    while (right < s.length()) {
+        p = left, q = right;
+        while (p < q) {
+            swap(s[p], s[q]);
+            p++;
+            q--;
+        }
+        left += 2 * k;
+        right += 2 * k;
+    }
+    right = s.size() - 1;
+    while (left < right) {
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+
+    return s;
+}
+
+string reverseWords(string s) {
+    int right = s.size() - 1;
+    vector<char> result;
+
+    while (right >= 0) {
+        if (s[right] == ' ') {
+            right--;
+        } else {
+            break;
+        }
+    }
+
+    while (right > 0) {
+        if (s[right] == ' ' && s[right - 1] == ' ') {
+            right--;
+        } else {
+            result.push_back(s[right]);
+            right--;
+        }
+    }
+
+    int p = 0, q = 1;
+    int tmp;
+    while (q < result.size()) {
+        while (q < result.size() && result[q] != ' ') {
+            q++;
+        }
+        tmp = q + 1;
+        q =  q - 1;
+        while (p < q) {
+            swap(result[p], result[q]);
+            p++;
+            q--;
+        }
+        q = tmp;
+        p = tmp;
+    }
+
+    return string(result.begin(), result.end());
+}
+
+int main() {
+    cout << reverseWords("12");
+    cout << "aaaa" << endl;
 }
